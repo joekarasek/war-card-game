35dfa92e5b60c29d98b32b9fe43b4aba4345a4f6
Joe Karasek <joseph.karasek@gmail.com>
2020-03-20 19:22:04 -0700

    feat: card with rank and suite

--- src/lib/card.js
+++ src/lib/card.js
@@ -1,11 +1,28 @@
+/**
+ * Card class
+ * Represents a traditional playing card with a suit and rank.
+ * Once constructed, these values can never change, therefore there are only getters available
+ */
 class Card {
   constructor(suit, rank) {
     this.suit = suit;
     this.rank = rank;
   }
 
+  /**
+   * Gets the Suit of the Card
+   * @returns {string}
+   */
   getSuit() {
     return this.suit;
+  }
+
+  /**
+   * Gets the Rank of the Card
+   * @returns {number}
+   */
+  getRank() {
+    return this.rank;
   }
 }
 
