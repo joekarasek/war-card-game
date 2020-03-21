28b65134e80f064d268cb298ac93736e85813463
Joe Karasek <joseph.karasek@gmail.com>
2020-03-20 19:11:16 -0700

    feat: basic wepback bundling

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Card from './lib/card';
+
+const firstCard = new Card('Hearts', 1);
+
+console.log(firstCard.getSuit());
---
+++ src/lib/card.js
@@ -0,0 +1,10 @@
+export default class Card {
+  constructor(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  getSuit() {
+    return this.suit;
+  }
+}
