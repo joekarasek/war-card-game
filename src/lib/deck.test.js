630b2dc5a4b0eaf52da545fd58f181ac8d7e579b
Joe Karasek <joseph.karasek@gmail.com>
2020-03-20 19:45:41 -0700

    test: Deck instantiation tests

--- src/lib/deck.test.js
+++ src/lib/deck.test.js
@@ -1,7 +1,27 @@
 const Deck = require('./deck');
 
-const testDeck = new Deck();
+const testDeckDefault = new Deck();
 
 test('Deck instantiated', () => {
-  expect(testDeck).toBeDefined();
+  expect(testDeckDefault).toBeDefined();
+});
+
+test('Deck default instantiated with four suits', () => {
+  expect(testDeckDefault.getNumberOfSuits()).toBe(4);
+});
+
+test('Deck default four suits are Hearts, Diamonds, Spades, Clubs', () => {
+  expect(testDeckDefault.getSuits()).toEqual([
+    "Hearts",
+    "Diamonds",
+    "Spades",
+    "Clubs"]);
+});
+
+test('Deck default instantiated with twelve ranks', () => {
+  expect(testDeckDefault.getNumberOfRanks()).toBe(13);
+});
+
+test('Deck default has an array of 52 cards', () => {
+  expect(testDeckDefault.getCards().length).toBe(52);
 });
